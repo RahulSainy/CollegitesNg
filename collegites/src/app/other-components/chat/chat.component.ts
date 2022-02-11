1ca0e8ddef1324bcdb77ed82cc49ff53a61f7e7d
RahulSainy <kradekidrahul@gmail.com>
2022-02-11 23:06:41 +0530

    postmodeladded

--- collegites/src/app/other-components/chat/chat.component.ts
+++ collegites/src/app/other-components/chat/chat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-chat',
@@ -7,33 +8,37 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-  cards = [
-    {
-      title: 'Post 1 Theseses',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
-      img: 'https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg'
-    },
-    {
-      title: 'Card Title 2',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
-    },
-    {
-      title: 'Card Title 3',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
-    },
-    {
-      title: 'Card Title 4',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
-    },
-
+  posts: Post[] = [
+    new Post('Post 1 Theseses','Some quick example text to build on the card title and make up the bulk of the card content','https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg'),
+    new Post('Post 1 Theseses','Some quick example text to build on the card title and make up the bulk of the card content','https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'),
+  
   ];
+  //   {
+  //     title: 'Post 1 Theseses',
+  //     description: 'Some quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card contentSome quick example text to build on the card title and make up the bulk of the card content',
+   
+  //     img: 'https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg'
+  //   },
+  //   {
+  //     title: 'Card Title 2',
+  //     description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+   
+  //     img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+  //   },
+  //   {
+  //     title: 'Card Title 3',
+  //     description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+   
+  //     img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+  //   },
+  //   {
+  //     title: 'Card Title 4',
+  //     description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+   
+  //     img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+  //   },
+
+  // ];
   msg: string;
   url: string | ArrayBuffer | null;
 
---
+++ collegites/src/app/other-components/chat/post.model.ts
@@ -0,0 +1,8 @@
+export class Post{
+  
+constructor(public title:string,public desc:string,public img:string){
+    this.title = title;
+    this.desc = desc;
+    this.img = img;
+}
+}
