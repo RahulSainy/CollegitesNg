1ab861df964c92dcaa59c2838b76036c69a50609
RahulSainy <kradekidrahul@gmail.com>
2022-02-17 01:30:57 +0530

    ecommerce

--- collegites/src/app/Ecommerce/ecommerce-login/ecommerce-login.component.ts
+++ collegites/src/app/Ecommerce/ecommerce-login/ecommerce-login.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+// import * as firebase from 'firebase/compat';
 @Component({
   selector: 'app-ecommerce-login',
   templateUrl: './ecommerce-login.component.html',
-  styleUrls: ['./ecommerce-login.component.css']
+  styleUrls: ['./ecommerce-login.component.css'],
 })
 export class EcommerceLoginComponent implements OnInit {
+  constructor(private afAuth: AngularFireAuth) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login() {
+    this.afAuth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
-
 }
